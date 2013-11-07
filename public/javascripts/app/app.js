d65685b15628eb6dbb9dbbdbe580fdcccbcdcb7f
Mike Kunkel <yourmom.tv@gmail.com>
2013-11-07 11:48:24 -0600

    client side app.js basics

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -2,8 +2,17 @@ $(document).ready(initialize);
 
 function initialize(){
   $(document).foundation();
+
+  $('#loginButton').on('click', clickLogin);
+  $('#registerButton').on('click', clickRegister);
+  $('#loginForm').on('submit', submitLogin);
+  $('#registerForm').on('submit', submitRegister);
+  $('#gameForm').on('submit', submitGame);
 }
 
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//----------AJAX-FUNCTIONS-----------------------------------------------------
 
 
 function submitAjaxForm(event, form, fn) {
@@ -40,4 +49,47 @@ function sendGenericAjaxRequest(url, data, verb, altVerb, event, fn, form){
   if(altVerb) options.data._method = altVerb;
   $.ajax(options);
   if(event) event.preventDefault();
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//----------HANDLERS-----------------------------------------------------------
+
+function clickLogin(event) {
+  hideParentRow(this);
+  showParentRow('#loginForm');
+}
+function clickRegister(event) {
+  hideParentRow(this);
+  showParentRow('#registerForm');
+}
+
+function submitLogin(event) {
+  submitAjaxForm(event, this, showGameForm);
+  hideParentRow(this);
+}
+
+function submitRegister(event) {
+  submitAjaxForm(event, this, showGameForm);
+}
+
+function submitGame(event) {
+
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//----------VIEW-FUNCTIONS-----------------------------------------------------
+
+function hideParentRow(element) {
+  $(element).closest('.row').hide();
+}
+
+function showParentRow(element) {
+  $(element).closest('.row').fadeIn();
+}
+
+function showGameForm(data, form) {
+  hideParentRow(form);
+  showParentRow('#gameForm');
 }
